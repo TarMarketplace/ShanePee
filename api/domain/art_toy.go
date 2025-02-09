0359697b1de337f9a044e8b1d759764527034b5e
JomnoiZ <kamanun.m@gmail.com>
2025-02-09 22:12:04 +0700

    feat: ad new art toy function & fix: art toy field name

--- api/domain/art_toy.go
+++ api/domain/art_toy.go
@@ -1,9 +1,22 @@
 package domain
 
 type ArtToy struct {
-	ID     int64   `json:"id" gorm:"primaryKey"`
-	Name   string  `json:"name" gorm:"not null"`
-	Detail string  `json:"detail" gorm:"not null"`
-	Price  float64 `json:"price" gorm:"not null"`
+	ID           int64   `json:"id" gorm:"primaryKey"`
+	Name         string  `json:"name" gorm:"not null"`
+	Description  string  `json:"description" gorm:"not null"`
+	Price        float64 `json:"price" gorm:"not null"`
+	Photo        *string `json:"photo"`
+	Availability bool    `json:"availability" gorm:"not null"`
 	// TODO: add more fields about review, rating, etc.
+}
+
+func NewArtToy(name string, description string, price float64, photo *string) *ArtToy {
+	return &ArtToy{
+		ID:           GenID(),
+		Name:         name,
+		Description:  description,
+		Price:        price,
+		Photo:        photo,
+		Availability: true,
+	}
 }

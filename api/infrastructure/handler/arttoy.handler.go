e6143171ef2ff2352df875717272f420070cc4d3
JomnoiZ <kamanun.m@gmail.com>
2025-02-10 20:48:25 +0700

    feat: create ArrayResponse & fix: edit handler

--- api/infrastructure/handler/arttoy.handler.go
+++ api/infrastructure/handler/arttoy.handler.go
@@ -18,11 +18,11 @@ func NewArtToyHandler(artToySvc service.ArtToyService) ArtToyHandler {
 }
 
 // @Summary		Get Art Toys
-// @Description	get all art toys
-// @Tags			Art Toy
+// @Description	Get all art toys
+// @Tags			Art toy
 // @Accept			json
 // @Produce		json
-// @Success		200		{object}	[]domain.ArtToy
+// @Success		200		{object}	ArrayResponse{data=[]domain.ArtToy}
 // @Failure		400		{object}	ErrorResponse
 // @Failure		404		{object}	ErrorResponse
 // @Router			/v1/art-toy [get]

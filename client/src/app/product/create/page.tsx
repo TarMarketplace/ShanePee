6f630d5f15a3ae26ed6fe1d062262ab5fdfd920c
Wasrek <w.wichada26@gmail.com>
2025-02-12 14:15:43 +0700

    feat: create and edit art toy pages (not done)

---
+++ client/src/app/product/create/page.tsx
@@ -0,0 +1,14 @@
+// 'use client'
+
+// import ProductForm from '../_components/product-form'
+
+// export default function CreateProductPage() {
+//   const handleCreate = async (data: any) => {
+//   }
+
+//   return (
+//     <main className="grid size-full grid-cols-1 place-items-center bg-white p-4 md:p-12">
+//         <ProductForm onSubmit={handleCreate} />
+//     </main>
+//   )
+// }

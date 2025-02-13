0df336c6642877913c45de36f1a9684c61978127
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2025-02-14 00:20:18 +0700

    fix: spotlight use stacked layout

--- api/cmd/api/docs.go
+++ api/cmd/api/docs.go
@@ -36,27 +36,22 @@ func AddSwagger(router *gin.Engine) {
 func AddSpotlight(router *gin.Engine) {
 	router.GET("/docs/spotlight", func(ctx *gin.Context) {
 		ctx.Data(http.StatusOK, "text/html; charset=utf-8", []byte(`<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="referrer" content="same-origin" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>Docs Example reference</title>
-    <!-- Embed elements Elements via Web Component -->
-    <link href="https://unpkg.com/@stoplight/elements@8.0.0/styles.min.css" rel="stylesheet" />
-    <script src="https://unpkg.com/@stoplight/elements@8.0.0/web-components.min.js"
-            integrity="sha256-yIhuSFMJJ6mp2XTUAb4SiSYneP3Qav8Uu+7NBhGJW5A="
-            crossorigin="anonymous"></script>
-  </head>
-  <body style="height: 100vh;">
-    <elements-api
-      apiDescriptionUrl="/openapi.yaml"
-      router="hash"
-      layout="stacked"
-      tryItCredentialsPolicy="same-origin"
-    />
-  </body>
-</html>`))
+		<html lang="en">
+		  <head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<title>Elements in HTML</title>
+			<script src="https://unpkg.com/@stoplight/elements/web-components.min.js"></script>
+			<link rel="stylesheet" href="https://unpkg.com/@stoplight/elements/styles.min.css">
+		  </head>
+		  <body style="height: 100vh;">
+			<elements-api
+			  apiDescriptionUrl="/openapi.yaml"
+			  router="hash"
+			  layout="sidebar"
+			/>
+		  </body>
+		</html>`))
 	})
 }
 

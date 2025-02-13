de4c544658480e99c3736b2f0b00b53f84703d25
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2025-02-14 00:04:46 +0700

    refactor: separate auth and user

--- api/domain/user.go
+++ api/domain/user.go
@@ -26,21 +26,6 @@ type PaymentMethod struct {
 	CardOwner  *string `json:"card_owner" example:"Freddy Mercury"`
 }
 
-type PasswordChangeRequest struct {
-	ID        int64  `gorm:"primaryKey"`
-	UserID    int64  `gorm:"not null"`
-	TokenHash string `gorm:"not null"`
-	User      User   `gorm:"constraint:OnDelete:CASCADE;"`
-}
-
-func NewPasswordChangeRequest(tokenHash string, userID int64) *PasswordChangeRequest {
-	return &PasswordChangeRequest{
-		ID:        GenID(),
-		UserID:    userID,
-		TokenHash: tokenHash,
-	}
-}
-
 func NewUser(email string, passwordHash string) *User {
 	return &User{
 		ID:           GenID(),
--- api/dto/user.dto.go
+++ api/dto/user.dto.go
@@ -78,18 +78,3 @@ func (b *UserUpdateBody) IntoMap() map[string]any {
 	}
 	return result
 }
-
-type LoginBody struct {
-	Email    string `json:"email" example:"johndoe@example.com"`
-	Password string `json:"password" example:"VerySecurePassword"`
-}
-
-type ChangePasswordBody struct {
-	RequestID   int64  `json:"request_id"`
-	Token       string `json:"token"`
-	NewPassword string `json:"new_password"`
-}
-
-type RequestPasswordChangeBody struct {
-	Email string `json:"email" example:"johndoe@example.com"`
-}

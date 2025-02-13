6739a9215ea7c8b80b5fdf2868c16e8000ccbbd0
SecretKr <kraiwit.wit@gmail.com>
2025-02-13 20:42:09 +0700

    feat: add fetchUser to UserContext

--- client/src/providers/user-provider/index.tsx
+++ client/src/providers/user-provider/index.tsx
@@ -1,6 +1,13 @@
 'use client'
 
-import { createContext, useContext, useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 import { toast } from 'sonner'
 
 import type { User } from '@/types/users'
@@ -10,43 +17,47 @@ import { env } from '@/env'
 export interface UserData {
   user: User | null
   setUser: (user: User | null) => void
+  fetchUser: () => void
 }
 
 const UserContext = createContext<UserData | undefined>(undefined)
 
 function UserProvider({ children }: { children?: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null)
+  const router = useRouter()
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await fetch(
-          `${env.NEXT_PUBLIC_BASE_API_URL}/auth/me`,
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-          }
-        )
+  const fetchUser = useCallback(async () => {
+    try {
+      const response = await fetch(`${env.NEXT_PUBLIC_BASE_API_URL}/auth/me`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      })
 
-        if (response.ok) {
-          const data = await response.json()
-          setUser(data)
-        }
-      } catch {
+      if (response.ok) {
+        const data = await response.json()
+        setUser(data)
+      } else if (response.status === 401) {
+        router.push('/login')
+      } else {
         toast.error('Something went wrong')
       }
+    } catch {
+      toast.error('Something went wrong')
     }
+  }, [router])
 
+  useEffect(() => {
     fetchUser()
-  }, [])
+  }, [fetchUser])
 
   return (
     <UserContext.Provider
       value={{
         user,
         setUser,
+        fetchUser,
       }}
     >
       {children}

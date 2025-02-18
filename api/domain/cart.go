ea3f226796130823482af45389910df473f7aa1a
JomnoiZ <kamanun.m@gmail.com>
2025-02-19 00:43:05 +0700

    fix: normalize cart model

--- api/domain/cart.go
+++ api/domain/cart.go
@@ -1,15 +1,27 @@
 package domain
 
 type Cart struct {
-	ID      int64   `json:"id" gorm:"primaryKey"`
-	ArtToys []int64 `json:"art_toys" gorm:"type:integer[]"` // array of art toy IDs
-	OwnerId int64   `json:"owner_id" gorm:"not null"`
+	ID      int64 `json:"id" gorm:"primaryKey" example:"97"`
+	OwnerId int64 `json:"owner_id" gorm:"not null" example:"97"`
+}
+
+type CartItem struct {
+	ID       int64 `json:"id" gorm:"primaryKey" example:"97"`
+	CartID   int64 `json:"cart_id" gorm:"not null" example:"97"`
+	ArtToyId int64 `json:"art_toy_id" gorm:"not null" example:"97"`
 }
 
 func NewCart(ownerId int64) *Cart {
 	return &Cart{
 		ID:      GenID(),
-		ArtToys: []int64{},
 		OwnerId: ownerId,
+	}
+}
+
+func NewCartItem(cartId int64, artToyId int64) *CartItem {
+	return &CartItem{
+		ID:       GenID(),
+		CartID:   cartId,
+		ArtToyId: artToyId,
 	}
 }

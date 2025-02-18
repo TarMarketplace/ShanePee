a549bc49b3e00257cc656ec09ac5849e575a6804
xappyyy <surin123.james@gmail.com>
2025-02-18 09:19:32 +0700

    refactor: rename change password endpoints to reset password

--- api/domain/error.go
+++ api/domain/error.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	// Known error
-	ErrUserNotFound                  error = errors.New("user not found")
-	ErrArtToyNotFound                error = errors.New("art toy not found")
-	ErrPasswordChangeRequestNotFound error = errors.New("password change request not found")
+	ErrUserNotFound                 error = errors.New("user not found")
+	ErrArtToyNotFound               error = errors.New("art toy not found")
+	ErrPasswordResetRequestNotFound error = errors.New("password reset request not found")
 )
--- api/infrastructure/email/debug.go
+++ api/infrastructure/email/debug.go
@@ -9,28 +9,28 @@ import (
 )
 
 type debugEmailSenderImpl struct {
-	defaultName                    string
-	defaultMailAddress             string
-	changePasswordFrontendEndpoint string
+	defaultName                   string
+	defaultMailAddress            string
+	resetPasswordFrontendEndpoint string
 }
 
 var _ service.EmailSender = &debugEmailSenderImpl{}
 
 func NewDebug(cfg config.Config) *debugEmailSenderImpl {
 	return &debugEmailSenderImpl{
-		defaultName:                    cfg.Email.Name,
-		defaultMailAddress:             cfg.Email.Address,
-		changePasswordFrontendEndpoint: cfg.ChangePasswordFrontendEndpoint,
+		defaultName:                   cfg.Email.Name,
+		defaultMailAddress:            cfg.Email.Address,
+		resetPasswordFrontendEndpoint: cfg.ResetPasswordFrontendEndpoint,
 	}
 }
 
-func (d *debugEmailSenderImpl) SendChangePasswordEmail(ctx context.Context, to string, token string, requestID int64) error {
-	resetLink := fmt.Sprintf("%s?token=%s&request_id=%d", d.changePasswordFrontendEndpoint, token, requestID)
+func (d *debugEmailSenderImpl) SendResetPasswordEmail(ctx context.Context, to string, token string, requestID int64) error {
+	resetLink := fmt.Sprintf("%s?token=%s&request_id=%d", d.resetPasswordFrontendEndpoint, token, requestID)
 
 	fmt.Printf("From: %s %s\n", d.defaultName, d.defaultMailAddress)
 	fmt.Printf("To: %s\n", to)
-	fmt.Printf("Subject: Change Password\n")
-	fmt.Printf("Body: Click the link to change your password: %s\n", resetLink)
+	fmt.Printf("Subject: Reset Password\n")
+	fmt.Printf("Body: Click the link to reset your password: %s\n", resetLink)
 
 	return nil
 }

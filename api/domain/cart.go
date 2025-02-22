abc790352cbc6cd5043faa8ef4130cc5ca7f1457
JomnoiZ <kamanun.m@gmail.com>
2025-02-22 20:49:46 +0700

    fix: add json:'-'

--- api/domain/cart.go
+++ api/domain/cart.go
@@ -4,8 +4,8 @@ type CartItem struct {
 	ID       int64  `json:"id" gorm:"primaryKey" example:"97"`
 	OwnerID  int64  `json:"owner_id" gorm:"not null" example:"97"`
 	ArtToyID int64  `json:"art_toy_id" gorm:"not null" example:"97"`
-	Owner    User   `gorm:"foreignKey:OwnerID;constraint:OnDelete:CASCADE;"`
-	ArtToy   ArtToy `gorm:"foreignKey:ArtToyID;constraint:OnDelete:CASCADE;"`
+	Owner    User   `json:"-" gorm:"foreignKey:OwnerID;constraint:OnDelete:CASCADE;"`
+	ArtToy   ArtToy `json:"-" gorm:"foreignKey:ArtToyID;constraint:OnDelete:CASCADE;"`
 }
 
 func NewCartItem(ownerID int64, artToyID int64) *CartItem {

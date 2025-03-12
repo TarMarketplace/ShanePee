837a3c82a7c5591b8497e0ca376250c0f451abf0
SecretKr <kraiwit.wit@gmail.com>
2025-03-12 22:06:12 +0700

    feat: add order card component

---
+++ client/src/components/order-card/index.stories.tsx
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { OrderCard } from '.'
+
+const meta = {
+  title: 'Derivatives/OrderCard',
+  component: OrderCard,
+  parameters: {
+    layout: 'padded',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof OrderCard>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    buyer_name: 'JomNoiz',
+    order: {
+      buyer_id: 1234,
+      created_at: '2024-08-01T00:00:00Z',
+      delivery_service: 'Shopee express',
+      id: 97,
+      seller_id: 4321,
+      status: 'PENDING',
+      tracking_number: 'SPX123456789',
+    },
+    order_items: [
+      {
+        $schema: 'https://example.com/schemas/ArtToy.json',
+        availability: true,
+        description: 'CRYBABY × Powerpuff Girls Series Figures',
+        id: 9007199254740991,
+        name: 'CRYBABY × Powerpuff Girls Series Figures',
+        owner_id: 9007199254740991,
+        photo: 'https://placehold.co/150x96.png',
+        price: 100,
+        release_date: '2021-01-01T00:00:00Z',
+      },
+      {
+        $schema: 'https://example.com/schemas/ArtToy.json',
+        availability: true,
+        description: 'CRYBABY × Powerpuff Girls Series Figures',
+        id: 9007199254740991,
+        name: 'CRYBABY × Powerpuff Girls Series Figures',
+        owner_id: 9007199254740991,
+        photo: 'https://placehold.co/150x96.png',
+        price: 100,
+        release_date: '2021-01-01T00:00:00Z',
+      },
+      {
+        $schema: 'https://example.com/schemas/ArtToy.json',
+        availability: true,
+        description: 'CRYBABY × Powerpuff Girls Series Figures',
+        id: 9007199254740991,
+        name: 'CRYBABY × Powerpuff Girls Series Figures',
+        owner_id: 9007199254740991,
+        photo: 'https://placehold.co/150x96.png',
+        price: 100,
+        release_date: '2021-01-01T00:00:00Z',
+      },
+    ],
+  },
+}
---
+++ client/src/components/order-card/order-status.tsx
@@ -0,0 +1,36 @@
+import { Text } from '../text'
+
+export interface OrderStatusProps {
+  status: 'PENDING' | 'SHIPPING' | 'COMPLETED'
+}
+
+const OrderStatus = ({ status }: OrderStatusProps) => {
+  switch (status) {
+    case 'PENDING':
+      return (
+        <div className='flex h-6 w-20 items-center justify-center rounded-xl border border-warning bg-warning-50'>
+          <Text variant='sm-semibold' className='text-center text-warning'>
+            รอจัดส่ง
+          </Text>
+        </div>
+      )
+    case 'SHIPPING':
+      return (
+        <div className='flex h-6 w-20 items-center justify-center rounded-xl border border-info bg-info-50'>
+          <Text variant='sm-semibold' className='text-center text-info'>
+            กำลังจัดส่ง
+          </Text>
+        </div>
+      )
+    case 'COMPLETED':
+      return (
+        <div className='flex h-6 w-20 items-center justify-center rounded-xl border border-success bg-success-50'>
+          <Text variant='sm-semibold' className='text-center text-success'>
+            จัดส่งสำเร็จ
+          </Text>
+        </div>
+      )
+  }
+}
+
+export { OrderStatus }

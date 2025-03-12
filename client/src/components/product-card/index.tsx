ebaa879f420bb5bc9303fe659031ef4b9e10de82
shalluv <leon555thai@gmail.com>
2025-03-12 22:40:10 +0700

    feat: art toy detail page

--- client/src/components/product-card/index.tsx
+++ client/src/components/product-card/index.tsx
@@ -5,26 +5,25 @@ import { Text } from '@/components/text'
 
 import type { ArtToy } from '@/generated/api'
 
-import { Badge } from '../badge'
-
 export interface ProductCardProps {
-  // TODO: remove any
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  product: ArtToy | any
+  product: ArtToy
 }
 
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <div className='aspect-[250/320] size-full max-w-64 overflow-hidden rounded-xl bg-card'>
       <div className='relative h-2/5 w-full'>
-        <Image src={product.image} alt={product.name} fill />
+        <Image src={product.photo ?? ''} alt={product.name} fill />
       </div>
       <div className='flex h-3/5 w-full flex-col justify-between p-3'>
         <div className='flex flex-col gap-2'>
           <Text variant='sm-semibold' desktopVariant='md-semibold'>
             {product.name}
           </Text>
-          {product.discount ? (
+          <Text variant='sm-regular' desktopVariant='md-regular'>
+            ฿{product.price}
+          </Text>
+          {/* {product.discount ? (
             <div className='flex flex-col'>
               <div className='flex items-center gap-2'>
                 <Text
@@ -50,7 +49,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
             <Text variant='sm-regular' desktopVariant='md-regular'>
               ฿{product.price}
             </Text>
-          )}
+          )} */}
         </div>
         <div className='flex items-center gap-4'>
           <div className='flex items-center'>
@@ -59,13 +58,13 @@ const ProductCard = ({ product }: ProductCardProps) => {
               className='size-5 text-warning md:size-6'
             />
             <Text variant='xs-regular' desktopVariant='sm-regular'>
-              {product.rating.toFixed(1)}
+              4.5
             </Text>
           </div>
           <div className='flex items-center text-grey-500'>
             <Icon icon='typcn:location' className='size-4 md:size-5' />
             <Text variant='xs-regular' desktopVariant='sm-regular'>
-              {product.location}
+              Bangkok
             </Text>
           </div>
         </div>

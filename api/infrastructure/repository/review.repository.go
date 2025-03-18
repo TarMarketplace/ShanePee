2dfab48393fdcb9131d1fb5ec0a3ea59ecdd00f4
Kamanun Maneesri <jomlovejen7956@gmail.com>
2025-03-19 02:10:41 +0700

    feat: get reviews of seller

--- api/infrastructure/repository/review.repository.go
+++ api/infrastructure/repository/review.repository.go
@@ -39,6 +39,14 @@ func (r *reviewRepositoryImpl) FindReviewByArtToyID(ctx context.Context, artToyI
 	return &review, nil
 }
 
+func (r *reviewRepositoryImpl) FindReviewBySellerID(ctx context.Context, sellerID int64) ([]*domain.Review, error) {
+	var reviews []*domain.Review
+	if err := r.db.Table("reviews").Joins("JOIN order_items ON order_items.art_toy_id = reviews.art_toy_id").Joins("JOIN orders ON orders.id = order_items.order_id").Where("orders.seller_id = ?", sellerID).Find(&reviews).Error; err != nil {
+		return nil, err
+	}
+	return reviews, nil
+}
+
 func (r *reviewRepositoryImpl) FindReviewerByArtToyID(ctx context.Context, artToyID int64) (*int64, error) {
 	var buyerID int64
 	if err := r.db.Table("orders").Select("orders.buyer_id").Joins("JOIN order_items ON order_items.order_id = orders.id").Where("order_items.art_toy_id = ?", artToyID).Scan(&buyerID).Error; err != nil {

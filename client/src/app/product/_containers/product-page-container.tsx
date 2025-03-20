f2b750f4d564d11b4b78677a1198aacf3ec4b127
SecretKr <kraiwit.wit@gmail.com>
2025-03-20 16:42:30 +0700

    fix: use change seller dialog and improve error handling

--- client/src/app/product/_containers/product-page-container.tsx
+++ client/src/app/product/_containers/product-page-container.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-import { Icon } from '@iconify/react/dist/iconify.js'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { toast } from 'sonner'
-
-import { Button } from '@/components/button'
-import { Text } from '@/components/text'
 
 import { useCart } from '@/providers/cart-provider'
 import { useUser } from '@/providers/user-provider'
@@ -14,6 +10,7 @@ import { useUser } from '@/providers/user-provider'
 import type { ArtToy } from '@/generated/api'
 import { addItemToCart, removeItemFromCart } from '@/generated/api'
 
+import { ChangeSellerDialog } from '../_components/change-seller-dialog'
 import { ProductPage } from '../_components/product-page'
 
 interface ProductPageProps {
@@ -24,19 +21,28 @@ export function ProductPageContainer({ product }: ProductPageProps) {
   const router = useRouter()
   const { user } = useUser()
   const { cartItems, fetchCart } = useCart()
-  const [showPopover, setShowPopover] = useState(false)
+  const [showDialog, setShowDialog] = useState(false)
   const [cartButtonLoading, setCartButtonLoading] = useState(false)
 
-  if (!product) return <p>Product not found.</p>
-
-  const isInCart = cartItems.some((item) => item.art_toy_id === product.id)
-  const isDifferentSeller =
-    cartItems.length > 0 && cartItems[0].owner_id !== product.owner_id
+  const isInCart = useMemo(() => {
+    return product
+      ? cartItems.some((item) => item.art_toy_id === product.id)
+      : false
+  }, [cartItems, product])
+  const isDifferentSeller = useMemo(() => {
+    return (
+      product &&
+      cartItems.length > 0 &&
+      cartItems[0].owner_id !== product.owner_id
+    )
+  }, [cartItems, product])
 
   const getOwnerName = (ownerId: number) => {
     // TODO: get owner name from owner id
-    return ownerId
+    return ownerId?.toString() || ''
   }
+
+  if (!product) return <p>Product not found.</p>
 
   const handleCartButton = async () => {
     if (!user) {
@@ -48,18 +54,24 @@ export function ProductPageContainer({ product }: ProductPageProps) {
     if (isInCart) {
       const cartItem = cartItems.find((item) => item.art_toy_id === product.id)
       if (cartItem?.id) {
-        await removeItemFromCart({
+        const { response } = await removeItemFromCart({
           path: { id: cartItem.id },
         })
-        toast.success('Removed from cart')
+        if (response.ok) toast.success('Removed from cart')
+        else toast.error('Failed to remove from cart')
       }
     } else {
       if (isDifferentSeller) {
-        setShowPopover(true)
+        setCartButtonLoading(false)
+        setShowDialog(true)
         return
       }
-      await addItemToCart({ body: { art_toy_id: product.id } })
-      toast.success('Added to cart successfully')
+      const { response } = await addItemToCart({
+        body: { art_toy_id: product.id },
+      })
+      console.log(response)
+      if (response.ok) toast.success('Added to cart successfully')
+      else toast.error('Failed to add to cart')
     }
     setCartButtonLoading(false)
     fetchCart()
@@ -70,10 +82,14 @@ export function ProductPageContainer({ product }: ProductPageProps) {
     for (const item of cartItems) {
       await removeItemFromCart({ path: { id: item.id } })
     }
-    await addItemToCart({ body: { art_toy_id: product.id } })
-    toast.success('Cart updated with new seller')
-    setShowPopover(false)
+    const { response } = await addItemToCart({
+      body: { art_toy_id: product.id },
+    })
+    if (response.ok) toast.success('Cart updated with new seller')
+    else toast.error('Failed to update cart')
+    setShowDialog(false)
     fetchCart()
+    setCartButtonLoading(false)
   }
 
   return (
@@ -84,49 +100,13 @@ export function ProductPageContainer({ product }: ProductPageProps) {
         isInCart={isInCart}
         cartButtonLoading={cartButtonLoading}
       />
-      {showPopover && (
-        <>
-          <div
-            className='fixed inset-0 bg-black/20'
-            onClick={() => setShowPopover(false)}
-          ></div>
-          <div className='fixed left-1/2 top-1/2 flex w-[354px] -translate-x-1/2 -translate-y-1/2 flex-col rounded bg-white p-4 shadow-lg md:w-[500px]'>
-            <div className='mb-4 flex items-center'>
-              <Text variant='heading-md' className='w-full'>
-                ต้องการจะเปลี่ยนร้านหรือไม่
-              </Text>
-              <Icon
-                icon='maki:cross'
-                className='size-5 cursor-pointer text-grey-500'
-                onClick={() => setShowPopover(false)}
-              />
-            </div>
-            <div className='flex flex-wrap gap-1'>
-              <Text variant='lg-regular' className=''>
-                คุณกำลังจะเปลี่ยนร้านจาก
-                <span className='mx-1 font-bold'>
-                  {getOwnerName(cartItems[0]?.owner_id)}
-                </span>
-                เป็น
-                <span className='mx-1 font-bold'>
-                  {getOwnerName(product.owner_id)}
-                </span>
-                <br />
-                หากคุณเปลี่ยนร้าน <br className='md:hidden' />
-                <span className='text-primary underline'>
-                  สินค้าในตะกร้าทั้งหมดของคุณจะหายไป!!
-                </span>
-                กรุณาตรวจสอบสินค้าในตะกร้าก่อนยืนยัน
-              </Text>
-            </div>
-            <div className='flex justify-end'>
-              <Button variant='filled' onClick={handleConfirmDifferentSeller}>
-                เปลี่ยนร้าน
-              </Button>
-            </div>
-          </div>
-        </>
-      )}
+      <ChangeSellerDialog
+        sellerNameFrom={getOwnerName(cartItems[0]?.owner_id)}
+        sellerNameTo={getOwnerName(product.owner_id)}
+        showDialog={showDialog}
+        setShowDialog={setShowDialog}
+        handleConfirmDifferentSeller={handleConfirmDifferentSeller}
+      />
     </>
   )
 }

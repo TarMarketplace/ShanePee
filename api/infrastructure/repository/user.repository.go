d93a087acd6ca90d9a458fa02151bf55e221aea1
Kamanun Maneesri <jomlovejen7956@gmail.com>
2025-03-24 00:12:57 +0700

    fix: handler calculating avg rating with 0 review

--- api/infrastructure/repository/user.repository.go
+++ api/infrastructure/repository/user.repository.go
@@ -55,7 +55,7 @@ func (u *userRepositoryImpl) FindUserByID(ctx context.Context, id int64) (*domai
 func (u *userRepositoryImpl) FindSellers(ctx context.Context) ([]*domain.User, error) {
 	var users []*domain.User
 	err := u.db.Model(&domain.User{}).
-		Select("*, users.id AS id, users.photo AS photo, AVG(reviews.rating) AS rating, COUNT(reviews.id) AS number_of_reviews").
+		Select("*, users.id AS id, users.photo AS photo, COALESCE(AVG(reviews.rating), 0) AS rating, COUNT(reviews.id) AS number_of_reviews").
 		Joins("LEFT JOIN art_toys on users.id = art_toys.owner_id").
 		Joins("LEFT JOIN reviews on reviews.art_toy_id = art_toys.id").
 		Group("users.id").
@@ -69,7 +69,7 @@ func (u *userRepositoryImpl) FindSellers(ctx context.Context) ([]*domain.User, e
 func (u *userRepositoryImpl) FindSellerByID(ctx context.Context, id int64) (*domain.User, error) {
 	var user *domain.User
 	err := u.db.Model(&domain.User{}).
-		Select("*, users.id AS id, users.photo AS photo, AVG(reviews.rating) AS rating, COUNT(reviews.id) AS number_of_reviews").
+		Select("*, users.id AS id, users.photo AS photo, COALESCE(AVG(reviews.rating), 0) AS rating, COUNT(reviews.id) AS number_of_reviews").
 		Joins("LEFT JOIN art_toys on users.id = art_toys.owner_id").
 		Joins("LEFT JOIN reviews on reviews.art_toy_id = art_toys.id").
 		Group("users.id").
